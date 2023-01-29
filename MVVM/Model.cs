d0710231453f8c791eab19ae732aa05bc561e5b3
user228 <forfarmcard@mail.ru>
2023-01-29 12:25:23 +0300

    Добавьте файлы проекта.

---
+++ MVVM/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    public static class Model
+    {
+        public static List<string> dataListOperationStr = new List<string>() { "Сложение", "Вычитание", "Умножение", "Деление" };
+        public static List<string> dataListOperationZnak = new List<string>() { "+", "-", "*", "/" };
+        public static int indexComboBox = -1;
+
+        public static string One;
+        public static string Two;
+        public static string result = "";
+    }
+}
---
+++ MVVM/ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<String> ComboChange
+        {
+            get
+            {
+                return Model.dataListOperationStr;
+            }
+        }
+
+        int cbIndex = -1;
+        public int IndexSelected // Cвойство для нахождения индекса выбранного в Combobox элемента
+        {
+            set
+            {
+                Model.indexComboBox = value;
+                cbIndex = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("CBIndex"));
+            }
+        }
+
+        public string CBIndex // Свойство для отображения выбранной арифметической операции
+        {
+            get
+            {
+                if (cbIndex == -1)
+                {
+                    return "";
+                }
+                else
+                {
+                    return Model.dataListOperationZnak[cbIndex];
+                }
+            }
+        }
+    }
+}
